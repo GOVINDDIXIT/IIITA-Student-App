1a05a43cb986dea5b283b8231d3f38f20ed6543d
Govind Dixit <32304546+GOVINDDIXIT@users.noreply.github.com>
2018-10-20 04:16:00 +0530

    Add detail and post adapter

---
+++ app/src/main/java/govind/iiita/app/DetailActivity.java
@@ -0,0 +1,54 @@
+package govind.iiita.app;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.support.v7.widget.Toolbar;
+
+
+public class DetailActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    Toolbar toolbar;
+    WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        progressBar = findViewById(R.id.progressbar);
+        toolbar = findViewById(R.id.toolbar);
+        webview = findViewById(R.id.webview);
+
+        setSupportActionBar(toolbar);
+        webview.setVisibility(View.INVISIBLE);
+        webview.getSettings().setJavaScriptEnabled(true);
+        //webview.getSettings().setBuiltInZoomControls(true);
+       // webview.getSettings().setUseWideViewPort(true);
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                //Toast.makeText(DetailActivity.this, "Loading...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+                webview.setVisibility(View.VISIBLE);
+                //Toast.makeText(DetailActivity.this,"Page loaded",Toast.LENGTH_SHORT).show();
+                String javaScript = "javascript:(function() { var a= document.getElementsByTagName('header');a[0].hidden='true';a=document.getElementsByClassName('page_body');a[0].style.padding='0px';})()";
+                webview.loadUrl(javaScript);
+            }
+        });
+        webview.loadUrl(getIntent().getStringExtra("url"));
+
+
+    }
+}
