4a72065232d09d7ef0c1743a56d3986386b9ab5b
Govind Dixit <32304546+GOVINDDIXIT@users.noreply.github.com>
2018-10-20 14:41:49 +0530

    Add faculty and update recreate about page

---
+++ app/src/main/java/govind/iiita/app/Faculty.java
@@ -0,0 +1,13 @@
+package govind.iiita.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Faculty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faculty);
+    }
+}
